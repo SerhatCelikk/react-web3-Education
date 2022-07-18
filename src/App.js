111832d30b9c7142f67a4d1cf0be9bc899e75cef
SerhatCelikk <90610606+SerhatCelikk@users.noreply.github.com>
2022-07-18 20:02:00 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useRandomUser } from './hooks/userandomUser';
 
 function App() {
+
+  const [value, setValue] = useState(5);
+
+  // const memo = useMemo(()=>{
+  //   return false;
+  // }, [value])
+
+  //const ref = useRef(null);
+
+  // const fn = useCallback(()=>{
+  //   return false;
+  // },[]);
+  const { state } = useRandomUser();
+
+  const renderEmail = () => {
+    if(!state) return null;
+    return state.email;
+  };
+  
   return (
     <div className="App">
-        <h1>Start</h1>
+        <h1 > {value} Start</h1>
+        <button >Button</button>
+        <h1>{renderEmail()}</h1>
     </div>
   );
 }
---
+++ src/hooks/userandomUser.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export const useRandomUser = ()=>{
+    const [state, setState] = useState(null);
+
+    useEffect(()=>{
+    const fn= async () => {
+        const result = await fetch("https://randomuser.me/api/");
+        const json =await result.json();
+         setState(json.results[0]);
+    }
+    fn();
+    },[])
+
+    return {state};
+};
